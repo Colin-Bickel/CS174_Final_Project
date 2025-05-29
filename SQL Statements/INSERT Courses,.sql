1d32d4459e9881cca9f176061530cc38ba68b614
Colin Bickel <cbickel@ucsb.edu>
2025-05-28 23:04:25 -0700

    added courses and students sql statements

---
+++ SQL Statements/INSERT Courses,.sql
@@ -0,0 +1,14 @@
+INSERT ALL
+    INTO COURSE (cno, enroll_code, title) VALUES ('CS174', 12345, NULL)
+    INTO COURSE (cno, enroll_code, title) VALUES ('CS170', 54321, NULL)
+    INTO COURSE (cno, enroll_code, title) VALUES ('CS160', 41725, NULL)
+    INTO COURSE (cno, enroll_code, title) VALUES ('CS026', 76543, NULL)
+    INTO COURSE (cno, enroll_code, title) VALUES ('EC154', 93156, NULL)
+    INTO COURSE (cno, enroll_code, title) VALUES ('EC140', 19023, NULL)
+    INTO COURSE (cno, enroll_code, title) VALUES ('EC015', 71631, NULL)
+    INTO COURSE (cno, enroll_code, title) VALUES ('CS154', 32165, NULL)
+    INTO COURSE (cno, enroll_code, title) VALUES ('CS130', 56789, NULL)
+    INTO COURSE (cno, enroll_code, title) VALUES ('EC152', 91823, NULL)
+    INTO COURSE (cno, enroll_code, title) VALUES ('CS010', 81623, NULL)
+    INTO COURSE (cno, enroll_code, title) VALUES ('EC010', 82612, NULL)
+SELECT * FROM dual;
---
+++ SQL Statements/INSERT Students.sql
@@ -0,0 +1,18 @@
+INSERT ALL
+    INTO STUDENT (perm, name, pin, address, mid) VALUES (12345, 'Alfred Hitchcock', 12345, '6667 El Colegio #40', 0)
+    INTO STUDENT (perm, name, pin, address, mid) VALUES (14682, 'Billy Clinton', 14682, '5777 Hollister', 1)
+    INTO STUDENT (perm, name, pin, address, mid) VALUES (37642, 'Cindy Laugher', 37642, '7000 Hollister', 0)
+    INTO STUDENT (perm, name, pin, address, mid) VALUES (85821, 'David Copperfill', 85821, '1357 State St', 0)
+    INTO STUDENT (perm, name, pin, address, mid) VALUES (38567, 'Elizabeth Sailor', 38567, '4321 State St', 1)
+    INTO STUDENT (perm, name, pin, address, mid) VALUES (81934, 'Fatal Castro', 81934, '3756 La Cumbre Plaza', 0)
+    INTO STUDENT (perm, name, pin, address, mid) VALUES (98246, 'George Brush', 98246, '5346 Foothill Av', 0)
+    INTO STUDENT (perm, name, pin, address, mid) VALUES (35328, 'Hurryson Ford', 35328, '678 State St', 1)
+    INTO STUDENT (perm, name, pin, address, mid) VALUES (84713, 'Ivan Lendme', 84713, '1235 Johnson Dr', 1)
+    INTO STUDENT (perm, name, pin, address, mid) VALUES (36912, 'Joe Pepsi', 36912, '3210 State St', 0)
+    INTO STUDENT (perm, name, pin, address, mid) VALUES (46590, 'Kelvin Coster', 46590, 'Santa Cruz #3579', 0)
+    INTO STUDENT (perm, name, pin, address, mid) VALUES (91734, 'Li Kung', 91734, '2 People''s Rd Beijing', 1)
+    INTO STUDENT (perm, name, pin, address, mid) VALUES (73521, 'Magic Jordon', 73521, '3852 Court Rd', 0)
+    INTO STUDENT (perm, name, pin, address, mid) VALUES (53540, 'Nam-hoi Chung', 53540, '1997 People''s St HK', 0)
+    INTO STUDENT (perm, name, pin, address, mid) VALUES (82452, 'Olive Stoner', 82452, '6689 El Colegio #151', 1)
+    INTO STUDENT (perm, name, pin, address, mid) VALUES (18221, 'Pit Wilson', 18221, '911 State St', 1)
+SELECT * FROM dual;
